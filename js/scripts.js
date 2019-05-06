b1a597be2cb9a99e3d864fe2dcc195d7e2530ea4
jonathanccardoso <jonathan99moura@hotmail.com>
2019-05-06 16:16:09 -0300

    Update styles index and internal pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,13 @@
 // Empty JS for your own code to be here
+
+function fonte(e) {
+    var elemento = $(".acessibilidade");
+    var fonte = parseInt(elemento.css('font-size'));
+
+    e == 'a' ? fonte++ : fonte--;
+
+    elemento.css("fontSize", fonte);
+}
+
+//search
+//https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_find2
