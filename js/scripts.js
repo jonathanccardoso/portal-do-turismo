58279be2056f0d723b6cfd46480328df81477c13
Jonathan Cardoso <jonathan.cardoso@academico.ifrn.edu.br>
2019-05-08 14:26:47 -0300

    Update search

--- js/scripts.js
+++ js/scripts.js
@@ -9,31 +9,34 @@ function fonte(e) {
     elemento.css("fontSize", fonte);
 }
 
-//search
 //https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_find2
 /*
-    <!-- <form name="searchForm" action="" method="post" onsubmit="return search()">
-        <input type="text" name="search" placeholder="Digite aqui..." id="search" value="teatro">
-    </form>
 
-    <p>Value: <span id="valueSearch"></span></p>
+<!DOCTYPE html>
+<html>
+<body>
 
-    <script>
-        var names = ['teatro', 'teatros'];
 
-        //var ages = [4, 12, 16, 20];
+<form name="searchForm" action="" method="post" onsubmit="return myFunction()">
+  <input type="text" name="search" placeholder="Digite aqui..." id="ageToCheck" value="teatro">
+</form>
 
-        function checkSearch(name) {
-            if (name == document.getElementById("search").value) {
-                //return window.location.href = "http://www.devmedia.com.br/guia/javascript/34372";
-                return name == document.getElementById("search").value;
-            }
-            //message error or focus in input form
-        }
+<p>Value: <span id="demo"></span></p>
 
-        function search() {
-            //var textSearch = document.forms["searchForm"]["search"].value;
-            document.getElementById("valueSearch").innerHTML = names.find(checkSearch);
-        }
+<script>
+var ages = ['teatro alberto maranhao', 'teatro', 'teatros'];
 
-    </script> -->*/
+function checkAdult(age) {
+  return age >= document.getElementById("ageToCheck").value;
+}
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = ages.find(checkAdult);
+}
+</script>
+
+</body>
+</html>
+
+
+*/
